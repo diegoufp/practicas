d4c13b95ed0b0d44cb879fb1923199e3ab0e2598
diegoufp <diegofernandezp001@gmail.com>
2020-07-20 23:36:37 -0500

    Se mejora el codigo del reto 14

--- reto14.py
+++ reto14.py
@@ -1,9 +1,19 @@
 def primos(limit):
 
-    if limit%2 != 0 and limit%3 != 0 and limit%limit == 0:
-        print(f'{limit} Este es un numero primo')
-    else:
-        print(f'{limit} Este no es un numero primo')
+    for x in range(2,limit + 1):
+        if x == 2:
+            print(x)
+        elif x == 3:
+            print(x)
+        elif x == 5:
+            print(x)
+        elif x == 7:
+            print(x)
+        else:
+            if x%2 != 0 and x%3 != 0 and x%5 != 0 and x%7 != 0 and x%x == 0:
+                print(x)
+            else:
+                continue
 
 if __name__ == "__main__":
     limit = int(input('Ingresa un numero limite para calcular los numero primos: '))
