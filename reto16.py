f44e30d9af5731a16598d0eeb85662d889d803ba
diegoufp <diegofernandezp001@gmail.com>
2020-07-22 19:31:47 -0500

    Se mejora el codigo del reto 16

--- reto16.py
+++ reto16.py
@@ -2,12 +2,15 @@ def calculadora(operation):
 
     try:
         result = eval(operation)
-        return result
     except (NameError, TypeError, SyntaxError) as e:
         print(f'{e}\n')
         print(f'No introdujiste un valor numérico, de operación o la la operacion esta mal escrita. Intenta de nuevo.')
+        return 0
     else:
-        return result
+        if type(result) == int or type(result) == float or type(result) == complex:
+            return result
+        else:
+            return 0
 
 if __name__ == '__main__':
     operation = input('Ingresa la operacion que quieres resolver: ')
